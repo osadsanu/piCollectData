1dfeffb6de413ab0ef878c1ff012983ec60c6faa
osdn <oadrian21@gmail.com>
2019-11-22 00:19:24 -0600

    add sh

--- installDependencies.sh
+++ installDependencies.sh
@@ -1,4 +1,7 @@
 sudo apt install golang git
 GO111MODULE=on
 mkdir collectData
+cd collectData
 git clone https://github.com/osadsanu/piCollectData.git . 
+echo "please run:    go run main.go -o serverCollect"
+echo "please execute ./serverCollect"
