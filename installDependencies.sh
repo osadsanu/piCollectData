d6d2b58e853c1878804750e7a2b595ab5722be37
osdn <oadrian21@gmail.com>
2019-11-22 18:59:39 -0600

    update installdependencies

--- installDependencies.sh
+++ installDependencies.sh
@@ -3,5 +3,6 @@ GO111MODULE=on
 mkdir collectData
 cd collectData
 git clone https://github.com/osadsanu/piCollectData.git . 
-echo "please run:    go run main.go -o serverCollect"
-echo "please execute ./serverCollect"
+echo "\t please run:    go to collectData directory: `cd collectData`"
+echo "\t please run:    go run main.go -o serverCollect"
+echo "\t please execute ./serverCollect"
